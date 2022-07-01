d8fd7ac7e1929bb636d39dc88febb13519d9f405
Alonso MD <alonso2295@hotmail.com>
2022-07-01 13:22:35 -0500

    First dag

---
+++ dags/user_processing.py
@@ -0,0 +1,25 @@
+from airflow import DAG
+from airflow.providers.postgres.operators.postgres import PostgresOperator
+from datetime import datetime
+
+with DAG('user_processing',
+        start_date=datetime(2022,6,30),
+        schedule_interval='@daily',
+        catchup=False
+    ) as dag:
+    None
+
+    create_table = PostgresOperator(
+        task_id='create_table',
+        postgres_conn_id='postgres',
+        sql='''
+            CREATE TABLE IF NOT EXISTS users (
+                firstname TEXT NOT NULL,
+                lastname TEXT NOT NULL,
+                country TEXT NOT NULL,
+                username TEXT NOT NULL,
+                password TEXT NOT NULL,
+                email TEXT NOT NULL
+            );
+        '''
+    )
