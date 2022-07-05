4cdafe1a2a394ebc8463616d9215b7334eb97896
Alonso Donayre <alonso2295@hotmail.com>
2022-07-04 20:38:36 -0500

    Nested task groups

--- dags/task_group.py
+++ dags/task_group.py
@@ -18,16 +18,23 @@ with DAG('taskgroup_dag', default_args=default_args, schedule_interval='@daily',
     )
 
     with TaskGroup('processing_tasks') as processing_tasks:
-         
         task_2 = BashOperator(
-            task_id='task_2',
-            bash_command='sleep 3'
-        )
+                task_id='task_2',
+                bash_command='sleep 3'
+            )
 
-        task_3 = BashOperator(
-            task_id='task_3',
-            bash_command='sleep 3'
-        )
+        # By default the taskgroup id is the prefix of the task.
+        with TaskGroup('spark_tasks') as spark_tasks:
+            task_2 = BashOperator(
+                task_id='task_3', # spark_tasks.task_3
+                bash_command='sleep 3'
+            )
+
+        with TaskGroup('flink_tasks') as spark_tasks:
+            task_3 = BashOperator(
+                task_id='task_3', #flink_tasks.task_3
+                bash_command='sleep 3'
+            )
 
 
     task_4 = BashOperator(
