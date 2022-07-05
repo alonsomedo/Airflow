802db19f4dd6e752667c83386b791989b86842f1
Alonso Donayre <alonso2295@hotmail.com>
2022-07-05 12:04:42 -0500

    Trigger rule - All failed

--- dags/trigger_rule.py
+++ dags/trigger_rule.py
@@ -13,20 +13,21 @@ with DAG('trigger_rule',
     
     task_1 = BashOperator(
         task_id='task_1',
-        bash_command='exit 0'
+        bash_command='exit 1',
         do_xcom_push=False
     )
 
     task_2 = BashOperator(
         task_id='task_2',
-        bash_command='exit 0'
+        bash_command='exit 1',
         do_xcom_push=False
     )
 
     task_3 = BashOperator(
         task_id='task_3',
-        bash_command='exit 0'
-        do_xcom_push=False
+        bash_command='exit 0',
+        do_xcom_push=False,
+        trigger_rule='all_failed'
     )
 
     [task_1, task_2] >> task_3
